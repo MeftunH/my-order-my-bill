480a2df508d8052b425f770dbadff87eb4cac422
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 08:46:47 +0300

    Init project

---
+++ src/main/java/com/example/myordermybill/MyOrderMyBillApplication.java
@@ -0,0 +1,13 @@
+package com.example.myordermybill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrderMyBillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOrderMyBillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myordermybill/MyOrderMyBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myordermybill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOrderMyBillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
