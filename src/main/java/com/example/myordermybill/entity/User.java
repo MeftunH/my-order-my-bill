16d8179e32ba6056f7e5f351df8044e561a10d5a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 22:15:14 +0300

    FEAT: add User Entity

---
+++ src/main/java/com/example/myordermybill/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.myordermybill.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name="USER")
+@Getter
+@Setter
+public class User {
+    @Id
+    @SequenceGenerator(name ="User" ,sequenceName = "USER_ID_SEQ")
+    @GeneratedValue(generator = "User")
+    private Long id;
+
+    @Column(name="NAME",length=30,unique=true,nullable = false)
+    private String NAME;
+
+    @Column(name="USERNAME",length=30,unique=true,nullable = false)
+    private String username;
+
+    @OneToMany(mappedBy="user")
+    private Set<Bill> bills;
+}
