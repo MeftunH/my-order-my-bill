ddfc854f843d0ea68e66eb31cdc0e6d7af6cd130
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-04 20:17:24 +0300

    BUILD: set bill controller

--- src/main/java/com/example/myordermybill/controller/BillController.java
+++ src/main/java/com/example/myordermybill/controller/BillController.java
@@ -1,4 +1,14 @@
 package com.example.myordermybill.controller;
 
+import com.example.myordermybill.request.BillSave;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/v1/bill")
 public class BillController {
+    @PostMapping("/create")
+    public void createBill(@RequestBody BillSave billSave) {
+
+    }
 }
