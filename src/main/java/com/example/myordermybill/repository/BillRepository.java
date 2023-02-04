9c7ac0f90f9c6256b8abe614510a675ed6781e28
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-04 20:18:11 +0300

    FEAT: add find bills by user functionality

--- src/main/java/com/example/myordermybill/repository/BillRepository.java
+++ src/main/java/com/example/myordermybill/repository/BillRepository.java
@@ -1,7 +1,12 @@
 package com.example.myordermybill.repository;
 
 import com.example.myordermybill.entity.Bill;
+import com.example.myordermybill.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface BillRepository extends JpaRepository<Bill,Long> {
+    List<Bill> findBillsByUser(User u);
 }
